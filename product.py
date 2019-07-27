7d66f914917fd0dd2cbc2e7a9dbd3ed66ec927ec
Buno Hsu <bunohsu@gmail.com>
2019-07-27 22:59:11 +0800

    product

--- product.py
+++ product.py
@@ -5,11 +5,16 @@ while True:
 	if name == 'q':
 		break
 	price = input('請輸入商品價格:')
-	p = []
+	p = [] # 也可以寫成 p = [name, price] 這樣8.9.10只要變一行就可以了
 	p.append(name)  #建立一個P清單，分別append name&price進去，然後再把P丟到最外面的products清單
 	p.append(price)
-	products.append(p)
+	products.append(p) #也可以寫成 products.append([name, prcie])這樣8.9.10都不用寫了
 print(products)
 
 print(products[0])
 print(products[0][0])
+
+#使用for loop篩選要的東西
+for p in products:
+	print(p) #印出每個小清單
+	print(p[0],'的價格是', p[1]) #因為是for loop會印出每個小清單的名稱&價格
